01f09da7a753828dc8b92be6379865e881ec9abc
Doug Lee <douglee650@gmail.com>
2025-06-22 06:57:22 -0700

    fix: remove absolute positioning

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -17,7 +17,7 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="absolute inset-x-0 top-0 z-50" id="top">
+    <header className="inset-x-0 top-0 z-50" id="top">
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <a href="#" className="-m-1.5 p-1.5">
           <span className="sr-only">Your Company</span>
